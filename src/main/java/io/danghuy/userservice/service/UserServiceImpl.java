4222b2a516d5e5ed59573f8e4e920365dd07e407
Vo Dang Huy <iamdaghuy@gmail.com>
2025-11-14 07:46:15 +0700

    create some logs (@Slf4j)

--- src/main/java/io/danghuy/userservice/service/UserServiceImpl.java
+++ src/main/java/io/danghuy/userservice/service/UserServiceImpl.java
@@ -21,24 +21,21 @@ public class UserServiceImpl implements UserService{
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
 
-    @Autowired
-    public UserServiceImpl(UserRepo userRepo, RoleRepo roleRepo) {
-        this.userRepo = userRepo;
-        this.roleRepo = roleRepo;
-    }
-
     @Override
     public User saveUser(User user) {
+        log.info("Saving new user {} to database", user.getName());
         return userRepo.save(user);
     }
 
     @Override
     public Role saveRole(Role role) {
+        log.info("Saving new role {} to database", role.getName());
         return roleRepo.save(role);
     }
 
     @Override
     public void addRoleToUser(String username, String roleName) {
+        log.info("Adding role {} to user {}", roleName, username);
         User user = userRepo.findByUsername(username);
         Role role = roleRepo.findByName(roleName);
         user.getRoles().add(role);
@@ -46,11 +43,13 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public User getUser(String username) {
+        log.info("Fetching user {}", username);
         return userRepo.findByUsername(username);
     }
 
     @Override
     public List<User> getUsers() {
+        log.info("Fetching all users");
         return userRepo.findAll();
     }
 }
