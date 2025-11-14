36f03a4cd909f2aebbf2f39db277d6bb179bf44b
Vo Dang Huy <iamdaghuy@gmail.com>
2025-11-14 09:14:20 +0700

    initialize data for database

--- src/main/java/io/danghuy/userservice/UserserviceApplication.java
+++ src/main/java/io/danghuy/userservice/UserserviceApplication.java
@@ -1,7 +1,14 @@
 package io.danghuy.userservice;
 
+import io.danghuy.userservice.model.Role;
+import io.danghuy.userservice.model.User;
+import io.danghuy.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
@@ -11,4 +18,33 @@ public class UserserviceApplication {
 		SpringApplication.run(UserserviceApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(UserService userService){
+		return  args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole((new Role(null, "ROLE_SUPER_ADMIN")));
+
+			userService.saveUser(new User(null, "Dang Huy", "huy", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Dang Khoi", "khoi", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Dang Thinh", "thinh", "1234", new ArrayList<>()));
+			userService.saveUser(new User(null, "Thanh Toan", "toan", "1234", new ArrayList<>()));
+
+			userService.addRoleToUser("huy", "ROLE_USER");
+			userService.addRoleToUser("huy", "ROLE_SUPER_ADMIN");
+			userService.addRoleToUser("khoi", "ROLE_USER");
+			userService.addRoleToUser("thinh", "ROLE_USER");
+			userService.addRoleToUser("thinh", "ROLE_USER");
+			userService.addRoleToUser("toan", "ROLE_ADMIN");
+			userService.addRoleToUser("toan", "ROLE_MANAGER");
+		};
+	}
+	/*
+		- @Bean - Spring se quan ly va tao ra bean nay
+
+		- CommandLineRunner - la mot interface cua Spring Boot. Khi ung dung khoi dong xong,
+		  Spring Boot se chay ham nay mot lan duy nhat.
+	 */
 }
+
